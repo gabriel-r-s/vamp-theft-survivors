0f2f3c5c9b2d15d4d70f36d8f0e918b9615d53d5
gabriel-r-s <author@email>
2025-09-16 09:24:46 -0300

    permitir bullet viver alem do target

--- scripts/player/player_bullet.gd
+++ scripts/player/player_bullet.gd
@@ -7,6 +7,7 @@ class_name PlayerBullet
 var target: WeakRef
 var damage: float = 6.0
 var speed: float = 200.0
+var dir: Vector2 = Vector2.LEFT
 
 func dmg(dmg_: float) -> PlayerBullet:
     self.damage = dmg_
@@ -25,15 +26,12 @@ func timeout(secs: float) -> PlayerBullet:
     return self
 
 func _ready() -> void:
-    prints(self, "new bullet")
     timer.start()
 
 func _physics_process(_delta: float) -> void:
     var target_ref = target.get_ref()
-    if target_ref == null:
-        queue_free()
-        return
-    var dir: Vector2 = Vector2(target_ref.position - self.position).normalized()
+    if target_ref != null:
+        dir = self.position.direction_to(target_ref.position)
     velocity = dir * speed
     move_and_slide()
 
@@ -41,8 +39,6 @@ func _on_timer_timeout() -> void:
     queue_free()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-    if is_queued_for_deletion():
-        return
     if body.has_method("get_hit"):
         body.get_hit(self.damage)
     queue_free()
